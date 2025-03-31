39e3400e286aa42a3ba99ecac145c07929d15d11
DJ <DIVYJ913@GMAIL.COM>
2025-03-31 20:43:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,11 @@ def gfg():
 	if request.method == "POST":
 		# getting input with query from  HTML form
 		query = request.form.get("text")
-		client = genai.Client(api_key="AIzaSyCNh3EOg-j1awvkPHBD5oNl9J0nRjfeDEY")
+		client = genai.Client(api_key="API-KEY")
 		response = client.models.generate_content(
 		    model="gemini-2.0-flash", contents="Can you help me visualise"+query+"dont use any bold or italic text and keep it under 60 words", 
 		)
 		response_text = response.text
-		#print(response.text)
 	return render_template("index.html", response_text=response.text)
 
 if __name__ == "__main__":
